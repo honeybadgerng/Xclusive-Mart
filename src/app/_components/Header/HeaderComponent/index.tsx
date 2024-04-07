b6c83e4b4648478982f6bdffaa00dd667acdd390
Honeybadger <rajimoshood200482@gmail.com>
2024-04-07 12:02:10 +0100

    updated navbar for desktop and mobile

--- src/app/_components/Header/HeaderComponent/index.tsx
+++ src/app/_components/Header/HeaderComponent/index.tsx
@@ -8,26 +8,22 @@ import Link from 'next/link'
 import { Header } from '../../../../payload/payload-types'
 // import { noHeaderFooterUrls } from '../../../constants'
 import { Gutter } from '../../Gutter'
-// import { HeaderNav } from '../Nav'
+import { HeaderNav } from '../Nav'
 
-// import classes from './index.module.scss'
+import classes from './index.module.scss'
+import MobileNav from '../MobileNav'
 
 const HeaderComponent = ({ header }: { header: Header }) => {
   // const pathname = usePathname()
 
   return (
-    <nav
-    // className={[classes.header, noHeaderFooterUrls.includes(pathname) && classes.hide]
-    //   .filter(Boolean)
-    //   .join(' ')}
-    >
-      <Gutter //className={classes.wrap}
-      >
+    <nav className={[classes.header]}>
+      <Gutter className={classes.wrap}>
         <Link href="/">
           <Image src="/logo-black.svg" alt="logo" width={170} height={50} />
         </Link>
-
-        {/* <HeaderNav header={header} /> */}
+        <HeaderNav header={header} />
+        <MobileNav header={header} />
       </Gutter>
     </nav>
   )
