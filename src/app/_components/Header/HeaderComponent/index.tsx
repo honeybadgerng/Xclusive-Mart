027ffcbee9288135fffd60ce8c4b9aa37a0d7887
Honeybadger <rajimoshood200482@gmail.com>
2024-04-07 12:11:05 +0100

    ude constants to hide navbar where it is not needed in the app like create account page

--- src/app/_components/Header/HeaderComponent/index.tsx
+++ src/app/_components/Header/HeaderComponent/index.tsx
@@ -3,10 +3,10 @@
 import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
-// import { usePathname } from 'next/navigation'
+import { usePathname } from 'next/navigation'
 
 import { Header } from '../../../../payload/payload-types'
-// import { noHeaderFooterUrls } from '../../../constants'
+import { noHeaderFooterUrls } from '../../../constants'
 import { Gutter } from '../../Gutter'
 import { HeaderNav } from '../Nav'
 
@@ -14,10 +14,14 @@ import classes from './index.module.scss'
 import MobileNav from '../MobileNav'
 
 const HeaderComponent = ({ header }: { header: Header }) => {
-  // const pathname = usePathname()
+  const pathname = usePathname()
 
   return (
-    <nav className={[classes.header]}>
+    <nav
+      className={[classes.header, noHeaderFooterUrls.includes(pathname) && classes.hide]
+        .filter(Boolean)
+        .join(' ')}
+    >
       <Gutter className={classes.wrap}>
         <Link href="/">
           <Image src="/logo-black.svg" alt="logo" width={170} height={50} />
